e517b35c22d85ded92a662edbcc325d149bdff5a
Supakorn <supakorn.seet@bumail.net>
2024-11-22 05:44:43 +0700

    fixed ultimate walk problem

--- Assets/Scripts/Identity/Player/OOPPlayer.cs
+++ Assets/Scripts/Identity/Player/OOPPlayer.cs
@@ -35,25 +35,28 @@ public class OOPPlayer : Character
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if(isUseultimateMoveNow == false)
         {
-            Move(Vector2.up);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Move(Vector2.down);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Move(Vector2.left);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Move(Vector2.right);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            UseFireStorm();
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Move(Vector2.up);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Move(Vector2.down);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Move(Vector2.left);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Move(Vector2.right);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                UseFireStorm();
+            }
         }
     }
 
